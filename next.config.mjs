bed23a62da084d798e8b8aa9548e766d7d4cf945
FSIF <fsif.official@gmail.com>
2026-02-12 03:18:07 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',  // <--- ★ここが重要！これを足してください
+  output: 'export',
+  basePath: '/CosmoBase', 
+
   typescript: {
     ignoreBuildErrors: true,
   },
