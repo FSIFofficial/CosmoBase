5a527dcec770cac996defb9bcb2118bd06c7c85c
FSIF <fsif.official@gmail.com>
2026-02-16 15:40:04 +0900

    Update page.tsx

--- app/value/page.tsx
+++ app/value/page.tsx
@@ -4,6 +4,17 @@ import { useEffect } from "react"
 import { Button } from "@/components/ui/button"
 import Header from "@/components/header"
 import Footer from "@/components/footer"
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: "Cosmo Baseが提供する価値", 
+  description: "参加することで得られる具体的なメリット",
+    // OGPも個別で上書き
+  openGraph: {
+    title: "Cosmo Baseが提供する価値 | Cosmo Base",
+    description: "参加することで得られる具体的なメリット",
+  },
+}
 
 export default function ValuePage() {
   useEffect(() => {
