89f7ded600a789e2eccbbac19522e87d6dc0aed8
FSIF <fsif.official@gmail.com>
2026-05-27 23:55:39 +0900

    Update page.tsx

--- app/value/page.tsx
+++ app/value/page.tsx
@@ -2,7 +2,7 @@ import { Metadata } from "next"
 import ValuePageContent from "./page.client"
 
 export const metadata: Metadata = {
-  title: "Cosmo Baseのサービス,
+  title: "Cosmo Baseのサービス"
   description: "宇宙に対する関心度に合わせて設計された4つのレベルのコンテンツを提供し、継続的な接点を創出します。",
   // OGPも個別で上書き
   openGraph: {
