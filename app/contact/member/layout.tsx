5e3defa97dea33620199c0c0e6c109fc38ffe387
FSIF <fsif.official@gmail.com>
2026-06-27 10:55:11 +0900

    Update layout.tsx

--- app/contact/member/layout.tsx
+++ app/contact/member/layout.tsx
@@ -3,6 +3,9 @@ import type { Metadata } from 'next';
 export const metadata: Metadata = {
   title: 'メンバー参加申請フォーム | 未来宇宙産業フォーラム(FSIF)',
   description: '未来宇宙産業フォーラム(FSIF)は、本気で宇宙産業と向き合い行動する学生を募集しています。面接エントリーはこちらから。',
+  icons: {
+    icon: '/CosmoBase/FSIF.png',
+  },
   openGraph: {
     title: 'FSIF メンバー参加申請フォーム',
     description: '未来宇宙産業フォーラム(FSIF)は、本気で宇宙産業と向き合い行動する学生を募集しています。面接エントリーはこちらから。',
