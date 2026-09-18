828eadd6d8be35eb607f7f2ab61d63b77d6d4f54
cosmo-base <cosmobase.official@gmail.com>
2026-09-18 15:18:25 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ interface StatsData {
 export default function Home() {
   const [stats, setStats] = useState<StatsData>({
     events: "10",         //開催イベント数
-    participants: "162",  //参加者数
+    participants: "197",  //参加者数
     collaborations: "6", //パートナー数
     media: "0",          //メディア掲載
   })
